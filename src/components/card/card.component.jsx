0955a8e76c57388afb6ed95919e4dd61d9a85076
Vasantha Kumari <vasantha.kumari-dv@metlife.com>
2021-06-17 16:20:23 +0530

    files

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = props => (
     <div className = "card-container">
         <img
          alt='monster' 
-         src={'https://robohash.org/9?set=set2'} 
+         src={'https://robohash.org/{props.monster.id}?set=set2'} 
         />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
